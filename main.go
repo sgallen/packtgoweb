35a4b19a46006d45ee900514101b601976904fbc
Scott Allen <scott.allen@juicemobile.com>
2016-01-25 13:55:15 +0000

    Display connection status to sqlite database (dev.db).
    - The expectation is that a sqlite3 database called, dev.db, exists in the root
      of the repository.

--- main.go
+++ main.go
@@ -4,14 +4,20 @@ import (
     "fmt"
     "net/http"
     "html/template"
+
+    "database/sql"
+    _ "github.com/mattn/go-sqlite3"
 )
 
 type Page struct {
     Name string
+    DBStatus bool
 }
 
 func main() {
     templates := template.Must(template.ParseGlob("templates/*"))
+
+    db, _ := sql.Open("sqlite3", "dev.db")
 
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         p := Page{Name: "Gopher"}
@@ -19,6 +25,7 @@ func main() {
         if name := r.FormValue("name"); name != "" {
             p.Name = name
         }
+        p.DBStatus = db.Ping() == nil
 
         if err := templates.ExecuteTemplate(w, "index", p); err != nil {
             http.Error(w, err.Error(), http.StatusInternalServerError)
